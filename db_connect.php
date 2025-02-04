48d05f979e23168937342d59451a82b7dd20c665
Crosswel <92092960+Crosswel@users.noreply.github.com>
2025-02-04 00:19:11 +0000

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";  
-$user = "root";      
-$pass = "";          
+$user = "web";      
+$pass = "web";          
 $dbname = "cartas";  
 
 $conn = new mysqli($host, $user, $pass, $dbname);
